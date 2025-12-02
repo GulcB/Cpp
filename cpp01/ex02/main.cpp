b9dad106c86ae869133609749a92b1bf0276abb8
Gülçin Bodur <gbodur@k1m07s03.42istanbul.com.tr>
2025-12-02 19:04:35 +0300

    started

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbodur <gbodur@student.42istanbul.com.t    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/02 14:29:18 by gbodur            #+#    #+#             */
+/*   Updated: 2025/12/02 14:41:53 by gbodur           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+using   std::string;
+using   std::cout;
+using   std::endl;
+
+int main (int argc, char **argv)
+{
+    string  *stringPTR;
+    string  stringREF;
+    string  init;
+    
+    init = "HI THIS IS BRAIN";
+    cout << init << endl;
+    
+    return (0);
+}
