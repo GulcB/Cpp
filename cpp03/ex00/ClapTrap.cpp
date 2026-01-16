5580cdb79282f1ac2ff73785333962d0da49a626
Gulcin <152058251+GulcB@users.noreply.github.com>
2026-01-16 22:54:36 +0000

    feat(cpp03/ex00): implement ClapTrap member functions and fix constructor. - Fix default constructor: initialize _name with empty string instead of self-reference. - Implement attack() method with energy and hit point checks. - Implement takeDamage() method to reduce hit points. - Implement beRepaired() method to restore hit points with energy cost.

--- cpp03/ex00/ClapTrap.cpp
+++ cpp03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: gbodur <gbodur@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/16 15:48:25 by gbodur            #+#    #+#             */
-/*   Updated: 2026/01/16 20:32:53 by gbodur           ###   ########.fr       */
+/*   Updated: 2026/01/16 22:49:47 by gbodur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 using std::cout;
 using std::endl;
 
-ClapTrap::ClapTrap() : _name(_name), _hitP(10), _energyP(10), _attackDamage(0)
+ClapTrap::ClapTrap() : _name(""), _hitP(10), _energyP(10), _attackDamage(0)
 {
 	cout << "ClapTrap default constructor called" << endl;
 }
 
 ClapTrap::~ClapTrap()
 {
-	cout << "ClapTrap" << _name << "destructor called" << endl;
+	cout << "ClapTrap destructor called" << endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap &clapTrap)
@@ -39,7 +39,6 @@ ClapTrap &ClapTrap::operator=(const ClapTrap &clapTrap)
 	cout << "ClapTrap" << _name << "copy assignment operator called" << endl;
 	if (this != &clapTrap)
 	{
-        this->_name = clapTrap._name;
 		this->_attackDamage = clapTrap._attackDamage;
 		this->_energyP = clapTrap._energyP;
 		this->_hitP = clapTrap._hitP;
@@ -49,15 +48,48 @@ ClapTrap &ClapTrap::operator=(const ClapTrap &clapTrap)
 
 void ClapTrap::attack(const string &target)
 {
-
+	if (_hitP == 0 || _energyP == 0)
+		cout << "Energy is not enough" << endl;
+	else if (_hitP > 0 && _energyP > 0)
+	{
+		cout << "ClapTrap "<< _name
+		<< " attacks " << target << ", causing "
+		<< _attackDamage << " points of damage!";
+		_energyP--;
+	}
+	else
+        cout << "ClapTrap has gone." << endl;
 }
 
 void ClapTrap::takeDamage(unsigned int amount)
 {
-
+	if(_hitP > 0)
+	{
+        cout << "ClapTrap " << _name
+		<< " taking damage: " << amount << endl;
+        _hitP -= amount;
+    }
+	else
+        cout << "ClapTrap has gone." << endl;
 }
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
-	
+	if (_hitP == 0)
+	{
+        cout << "ClapTrap " << _name << " cannot be alive, it has gone." << endl;
+		return ;
+	}
+    if (_energyP == 0)
+	{
+		cout << "ClapTrap " << _name << " cannot be alive, it has gone." << endl;
+		return ;
+	}
+	else
+		cout << "ClapTrap " << _name
+		<< " repairing it self adding with "
+		<< amount << endl;
+	_energyP--;
+    _hitP += amount;
+    cout << "New Hit Point is: " << _hitP << endl;
 }
