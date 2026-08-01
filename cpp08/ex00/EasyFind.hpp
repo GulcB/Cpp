a045b9a27da9008658b4e5db8455cd5bb20198e6
Gülçin Bodur <gbodur@k1m08s03.42istanbul.com.tr>
2026-08-01 17:08:29 +0300

    feat(cpp08/ex00): Wrote Easyfind function template.

--- cpp08/ex00/EasyFind.hpp
+++ cpp08/ex00/EasyFind.hpp
@@ -6,12 +6,34 @@
 /*   By: gbodur <gbodur@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 09:09:22 by gbodur            #+#    #+#             */
-/*   Updated: 2026/07/22 17:20:07 by gbodur           ###   ########.fr       */
+/*   Updated: 2026/08/01 15:31:50 by gbodur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+# include <algorithm>
+# include <vector>
+# include <list>
+# include <iostream>
+
+using std::find;
+
+template<typename T>
+T::iterator EasyFind(T container, int index)
+{
+	if(container.empty())
+		return ;
+	for (int i = 0; i < container.size(); i++)
+	{
+		int search = find(container.begin(), container.end(), index);
+		if(search == index)
+			return (index);
+		else
+			
+		
+	}
+}
 
 #endif
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -6,13 +6,34 @@
 /*   By: gbodur <gbodur@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 09:09:18 by gbodur            #+#    #+#             */
-/*   Updated: 2026/07/22 17:20:17 by gbodur           ###   ########.fr       */
+/*   Updated: 2026/08/01 15:37:14 by gbodur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "EasyFind.hpp"
+#include <sstream>
+
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::vector;
+using std::list;
+using std::stringstream;
 
 int main(int argc, char **argv)
 {
+	if (argc != 2)
+	{
+		cerr << "Error: Invalid number of arguments." << endl;
+		cerr << "Usage: ./easyfind <numbers in vector> <value>" << endl;
+		return (1);
+	}
+	vector<int> containerVector;
+	stringstream ss;
+	int values;
+	ss << argv[1];
+	while (ss >> values)
+		containerVector.insert();
+	
 	
 }
