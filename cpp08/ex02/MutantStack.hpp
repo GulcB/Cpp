d72bebde9bfe8092b0a1aac4261a827846592ad1
Gulcin <152058251+GulcB@users.noreply.github.com>
2026-08-23 10:01:58 +0000

    feat(cpp08/ex02): Implement MutantStack and dynamic argument testing
    - Add the MutantStack class that inherits from std::stack to make it iterable.
    - Expose the protected underlying container to support standard, constant, and reverse iterators.
    - Comply with the Orthodox Canonical Form.
    - Update main.cpp to include the mandatory subject tests and a direct comparison with std::list.
    - Introduce dynamic argument parsing to push command-line inputs into the MutantStack and print them using custom iterators, ensuring unique code structure and avoiding unused parameter warnings.

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -6,14 +6,48 @@
 /*   By: gbodur <gbodur@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 09:11:01 by gbodur            #+#    #+#             */
-/*   Updated: 2026/07/22 17:15:10 by gbodur           ###   ########.fr       */
+/*   Updated: 2026/08/23 07:49:41 by gbodur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
-#include <algorithm>
+# include <stack>
+# include <deque>
 
+template <typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+	public:
+	    MutantStack() : std::stack<T, Container>() {}
+	    MutantStack(const MutantStack& other) : std::stack<T, Container>(other) {}
+	
+	    MutantStack& operator=(const MutantStack& other)
+	    {
+	        if (this != &other)
+	            std::stack<T, Container>::operator=(other);
+	        return *this;
+	    }
+	
+	    virtual ~MutantStack() {}
+	
+	    typedef typename Container::iterator iterator;
+	    typedef typename Container::const_iterator const_iterator;
+	    typedef typename Container::reverse_iterator reverse_iterator;
+	    typedef typename Container::const_reverse_iterator const_reverse_iterator;
+	
+	    iterator begin() { return this->c.begin(); }
+	    iterator end() { return this->c.end(); }
+	
+	    const_iterator begin() const { return this->c.begin(); }
+	    const_iterator end() const { return this->c.end(); }
+	
+	    reverse_iterator rbegin() { return this->c.rbegin(); }
+	    reverse_iterator rend() { return this->c.rend(); }
+	
+	    const_reverse_iterator rbegin() const { return this->c.rbegin(); }
+	    const_reverse_iterator rend() const { return this->c.rend(); }
+};
 
 #endif
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -6,14 +6,99 @@
 /*   By: gbodur <gbodur@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 09:10:57 by gbodur            #+#    #+#             */
-/*   Updated: 2026/07/22 17:17:07 by gbodur           ###   ########.fr       */
+/*   Updated: 2026/08/23 08:04:50 by gbodur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+#include <iostream>
+#include <list>
+#include <cstdlib>
 
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::list;
+using std::stack;
 
 int main(int argc, char **argv)
 {
-	
+    cout << "--- 1. Subject Mandatory Test ---" << endl;
+    MutantStack<int> mstack;
+    
+    mstack.push(5);
+    mstack.push(17);
+    cout << "Top element: " << mstack.top() << endl;
+    
+    mstack.pop();
+    cout << "Size after pop: " << mstack.size() << endl;
+    
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    
+    ++it;
+    --it;
+    
+    cout << "Stack contents:" << endl;
+    while (it != ite)
+    {
+        cout << *it << endl;
+        ++it;
+    }
+    stack<int> s(mstack);
+
+    cout << "\n--- 2. std::list Comparison Test ---" << endl;
+    list<int> mlist;
+    mlist.push_back(5);
+    mlist.push_back(17);
+    cout << "Back element: " << mlist.back() << endl;
+    
+    mlist.pop_back();
+    cout << "Size after pop_back: " << mlist.size() << endl;
+    
+    mlist.push_back(3);
+    mlist.push_back(5);
+    mlist.push_back(737);
+    mlist.push_back(0);
+
+    list<int>::iterator lit = mlist.begin();
+    list<int>::iterator lite = mlist.end();
+    
+    ++lit;
+    --lit;
+    
+    cout << "List contents:" << endl;
+    while (lit != lite)
+    {
+        cout << *lit << endl;
+        ++lit;
+    }
+
+    cout << "\n--- 3. Custom Argument Test ---" << endl;
+    if (argc > 1)
+    {
+        MutantStack<int> argStack;
+        for (int i = 1; i < argc; ++i)
+        {
+            argStack.push(std::atoi(argv[i]));
+        }
+
+        cout << "Arguments pushed to MutantStack. Elements:" << endl;
+        for (MutantStack<int>::iterator argIt = argStack.begin(); argIt != argStack.end(); ++argIt)
+        {
+            cout << *argIt << " ";
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << "No additional arguments provided. Run with './ex02 <numbers>' to test dynamic inputs." << endl;
+    }
+
+    return 0;
 }
