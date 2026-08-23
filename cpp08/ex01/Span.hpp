df73553c698649d47b5c50c1813ae4bbd962a30f
Gülçin Bodur <gbodur@k1m07s07.42istanbul.com.tr>
2026-08-23 20:46:11 +0300

    refactor(cpp08/ex01): exception class move to up

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: gbodur <gbodur@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 09:10:52 by gbodur            #+#    #+#             */
-/*   Updated: 2026/08/23 06:36:38 by gbodur           ###   ########.fr       */
+/*   Updated: 2026/08/23 13:35:15 by gbodur           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,18 @@ class Span
 	    std::vector<int>    _data;
 	
 	public:
+		class SpanFullException : public std::exception
+	    {
+	    	public:
+	        	virtual const char* what() const throw();
+	    };
+	
+	    class SpanNotEnoughNumbersException : public std::exception
+	    {
+	    	public:
+	        	virtual const char* what() const throw();
+	    };
+		
 	    Span();
 	    Span(unsigned int N);
 	    Span(const Span &other);
@@ -36,7 +48,7 @@ class Span
 	    template <typename Iterator>
 	    void addNumber(Iterator begin, Iterator end)
 	    {
-	        unsigned int distance = std::distance(begin, end);
+	        unsigned int distance = static_cast<unsigned int>(std::distance(begin, end));
 	        if (_data.size() + distance > _maxSize)
 	            throw SpanFullException();
 	        _data.insert(_data.end(), begin, end);
@@ -44,18 +56,6 @@ class Span
 	
 	    unsigned int shortestSpan() const;
 	    unsigned int longestSpan() const;
-	
-	    class SpanFullException : public std::exception
-	    {
-	    	public:
-	        	virtual const char* what() const throw();
-	    };
-	
-	    class SpanNotEnoughNumbersException : public std::exception
-	    {
-	    	public:
-	        	virtual const char* what() const throw();
-	    };
 };
 
 #endif
